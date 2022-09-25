516c74dc5dd84dd7973ddb318c20857745cc6e9a
Deunsol Park <deunsol916@gmail.com>
2022-09-25 23:58:15 +0900

    FEAT: JPA one-way association 1. 단방향 연관관계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,26 +14,22 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member1 = new Member();
-            member1.setUsername("A");
 
-            Member member2 = new Member();
-            member2.setUsername("B");
+            // 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
 
-            Member member3 = new Member();
-            member3.setUsername("C");
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);  // 영속성으로 인해 위에서 persist()한 team의 id를 가져올 수 있다.
+            em.persist(member);
 
-            System.out.println("=================");
+            Member findMember = em.find(Member.class, member.getId());
 
-            em.persist(member1);  // 1, 51
-            em.persist(member2);  // MEM
-            em.persist(member3);  // MEM
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
 
-            System.out.println("member1 = " + member1.getId());
-            System.out.println("member2 = " + member2.getId());
-            System.out.println("member3 = " + member3.getId());
-
-            System.out.println("=================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
