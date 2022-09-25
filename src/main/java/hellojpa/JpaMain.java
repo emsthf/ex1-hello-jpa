c0a1af2772c68e265b3d1e07858df84f6b9c669c
Deunsol Park <deunsol916@gmail.com>
2022-09-25 20:37:09 +0900

    FEAT: JPA Sequence mapping 1. JPA 기본 값 맵핑 중 시퀀스의 사용법과 원리를 공부

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,16 +14,26 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)  // 5번부터
-                    .setMaxResults(8)   // 8개 가져오라는 뜻
-                    .getResultList();
+            Member member1 = new Member();
+            member1.setUsername("A");
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            Member member2 = new Member();
+            member2.setUsername("B");
 
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+            System.out.println("=================");
+
+            em.persist(member1);  // 1, 51
+            em.persist(member2);  // MEM
+            em.persist(member3);  // MEM
+
+            System.out.println("member1 = " + member1.getId());
+            System.out.println("member2 = " + member2.getId());
+            System.out.println("member3 = " + member3.getId());
+
+            System.out.println("=================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,14 +1,24 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", // 맵핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
-    private String name;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    public Member() {
+    }
 
     public Long getId() {
         return id;
@@ -18,11 +28,11 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
